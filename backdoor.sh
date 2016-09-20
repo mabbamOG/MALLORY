ddbe5dd836f305f4d7bffb659b5791207470edb4
M.A.B <mariobarbara@tutanota.de>
2016-09-20 19:07:08 +0200

    added functionality, refactored code
    - hardened, refactored and enhanced `getdata` function
        - added upnp functionality !!
    - refactored code around slightly

--- backdoor.sh
+++ backdoor.sh
@@ -55,39 +55,46 @@ exit
 
 
 #-----------------------------MAIN FUNCTIONS:------------------------------------------------
-alias ip_private="ip route get 1 | sed -r -n 's/.*src ([0-9.:]+).*/\1/p')" # long live sed
-alias  ip_router="ip route get 1 | sed -r -n 's/.*via ([0-9.:]+).*/\1/p')"
-alias  ip_public="curl -s ipecho.net/plain" # -s kills progress output
 
 function getdata # Gather Statistics
 {
-echo -e "IP PRIVATE\t   - $(ip_private)" # long live sed
-echo -e "IP ROUTER\t    - $(ip_router)"
-echo -e "IP PUBLIC\t    - $(ip_public)" # -s kills progress output
-systemctl | grep $sshservice &>/dev/null && ssh='ENABLED' || ssh='DISABLED'
-echo -e "SSH\t          -> $ssh"
+# GET DATA
+    ip_private=$(ip route get 1 | sed -r -n 's/.*src (([0-9]{1,3}[.]){3,3}[0-9]{1,3}).*/\1/p') # long live sed
+    ip_router=$(ip route get 1 | sed -r -n 's/.*via (([0-9]{1,3}[.]){3,3}[0-9]{1,3}).*/\1/p')
+    ip_public=$(curl -s ipecho.net/plain) # -s kills progress output
+    systemctl | grep -F $sshservice &>/dev/null && ssh='ENABLED' || ssh='DISABLED'
+    upnpc -P | grep -F -i 'found valid igd' &>/dev/null && upnp='ENABLED' || upnp='DISABLED'
+    [[ $upnp == 'ENABLED' ]] && upnpc -l | grep -E "TCP\s*2222->${ip_private}:22" &>/dev/null && port='OPEN' || port='CLOSED'
+
+# ECHO DATA
+    echo -e "IP PRIVATE\t   - $ip_private" # long live sed
+    echo -e "IP ROUTER\t    - $ip_router"
+    echo -e "IP PUBLIC\t    - $ip_public" # -s kills progress output
+    echo -e "SSH\t          -> $ssh"
+    echo -e "UPNP\t         -> $upnp"
+    echo -e "PORT\t         -> $port"
 }
 
 function run
 {
 # IF NETWORK IS DOWN, ABORT
-ip route get 1 2>&1 | grep -i 'unreachable' &>/dev/null && exit # abort if down
+    ip route get 1 2>&1 | grep -i 'unreachable' &>/dev/null && exit # abort if down
 
 # CHECK NETWORK OR SSH CHANGES
-data=$(getdata)
-[[ -f $backdoor ]] && oldhash=$(cat $backdoor) || oldhash="" # get hash value, if it exists
-newhash=$(echo "$data" | md5sum) # hash network info
-[[ $oldhash == $newhash ]] && exit
+    data=$(getdata)
+    [[ -f $backdoor ]] && oldhash=$(cat $backdoor) || oldhash="" # get hash value, if it exists
+    newhash=$(echo "$data" | md5sum) # hash network info
+    [[ $oldhash == $newhash ]] && exit
+    echo "$newhash" >$backdoor
 
 # NETWORK HAS CHANGED:
-echo "$newhash" >$backdoor
-# 1. send email because pub ip might have changed, or ssh is down
-# (can verify with `mailq`)
-echo -e "$(date)\n$data" | mail -s 'BACKDOOR' $emailaddress # requires postfix or sendmail daemon
-# 2. update upnp mapping because priv ip might have changed (dhcp), or new network (pub ip change)
-# (can verify with `upnpc -l`)
-upnpc -d 2222 TCP # requires miniupnp
-upnpc -a "$(ip_private)" 22 2222 TCP
+    # 1. send email
+    # (can verify with `mailq`)
+    echo -e "$(date)\n$data" | mail -s 'BACKDOOR' $emailaddress # requires postfix or sendmail daemon
+    # 2. update upnp mapping (because dhcp change, or new pub ip, or upnp port not yet open)
+    # (can verify with `upnpc -l`)
+    upnpc -d 2222 TCP # requires miniupnp
+    upnpc -a $ip_private 22 2222 TCP
 }
 
 
